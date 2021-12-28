db3f4ddf1cf3cbcbbd3a5c3c7e71a4edae7b401e
Carlos Eduardo <93012183+EduardoGomes704@users.noreply.github.com>
2021-12-27 23:46:30 -0300

    jogo da velha

---
+++ Projetos/Jogo_da_velha.py
@@ -0,0 +1,39 @@
+from tk import *
+from tkinter import *
+from tkinter import ttk
+# cores ---------------------------------------
+co0 = "#FFFFFF"  # branca / white
+co1 = "#333333"  # preta pesado / dark black
+co2 = "#fcc058"  # laranja / orange
+co3 = "#38576b"  # valor / value
+co4 = "#3297a8"   # azul / blue
+co5 = "#fff873"   # amarela / yellow
+co6 = "#fcc058"  # laranja / orange
+co7 = "#e85151"   # vermelha / red
+co8 = co4   # + verde
+co10 = "#fcfbf7"
+fundo = "#3b3b3b" # preta / black
+
+# criando janela principal
+janela = Tk()
+janela.title('')
+janela.geometry('260x370')
+janela.configure(bg=fundo)
+
+janela.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
